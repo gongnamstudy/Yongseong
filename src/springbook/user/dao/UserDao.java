4e3f62e60aa7a564c1fdfadd5885e8806fb40ea7
Yong-Seong, KIM <seong2035@gmail.com>
2019-03-03 16:44:49 +0900

    Chap 01 - final

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -7,15 +7,17 @@ import java.sql.SQLException;
 
 import springbook.user.domain.User;
 
-public class UserDao {
-    private ConnectionMaker connectionMaker;
+import javax.sql.DataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+public class UserDao {
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+    public void add(User user) throws SQLException {
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
@@ -29,8 +31,8 @@ public class UserDao {
         c.close();
     }
 
-    public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+    public User get(String id) throws SQLException {
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?");
--- src/springbook/user/dao/UserDaoTest.java
+++ src/springbook/user/dao/UserDaoTest.java
@@ -2,12 +2,15 @@ package springbook.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao dao = new UserDao(connectionMaker);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationConfig.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("whiteship");
