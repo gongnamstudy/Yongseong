5e7179ddb39b3c468e604ff6dc08f0a5450d5369
Yong-Seong, KIM <seong2035@gmail.com>
2019-03-24 23:58:08 +0900

    Chap 05 - 5.1.1

--- src/springbook/user/dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package springbook.user.dao;
-
-import java.sql.SQLException;
-import java.sql.Connection;
-
-public interface ConnectionMaker {
-    public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
