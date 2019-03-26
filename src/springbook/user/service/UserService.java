b79b301fd62490de61e1647830a441e8fbe69c27
Yong-Seong, KIM <seong2035@gmail.com>
2019-03-27 00:17:34 +0900

    Chap 05 - 5.1.5

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -9,6 +9,9 @@ import java.util.List;
 public class UserService {
     UserDao userDao;
 
+    public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+    public static final int MIN_RECCOMEND_FOR_GOLD = 50;
+
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
@@ -16,22 +19,31 @@ public class UserService {
     public void upgradeLevels() {
         List<User> users = userDao.getAll();
         for (User user : users) {
-            Boolean changed = null;
-            if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-                user.setLevel(Level.SILVER);
-                changed = true;
-            } else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-                user.setLevel(Level.GOLD);
-                changed = true;
-            } else if (user.getLevel() == Level.GOLD) {
-                changed = false;
-            } else {
-                changed = false;
-            }
-
-            if (changed) {
-                userDao.update(user);
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
             }
         }
+    }
+
+    public void add(User user) {
+        if (user.getLevel() == null) {
+            user.setLevel(Level.BASIC);
+        }
+        userDao.add(user);
+    }
+
+    private boolean canUpgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+        switch (currentLevel) {
+            case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+            case SILVER: return (user.getRecommend() >= MIN_RECCOMEND_FOR_GOLD);
+            case GOLD: return false;
+            default: throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+        }
+    }
+
+    private void upgradeLevel(User user) {
+        user.upgradeLevel();
+        userDao.update(user);
     }
 }
